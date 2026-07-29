4ea718822ae7d5f7b8fc771e544c4547901f1803
F6CYK (Tony) <45797103+F6CYK@users.noreply.github.com>
2026-07-30 00:25:38 +0200

    Implement mobile menu toggle on button click
    Add event listener for mobile menu toggle functionality.

--- assets/js/mobile-navigation.js
+++ assets/js/mobile-navigation.js
@@ -1,6 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
 
-    // Navigation mobile
-    // À compléter
+    const bouton = document.querySelector(".menu-mobile-toggle");
+    const menu = document.querySelector("nav.menu-general");
+
+    if (!bouton || !menu)
+        return;
+
+    bouton.addEventListener("click", () => {
+
+        menu.classList.toggle("menu-ouvert");
+
+    });
 
 });
